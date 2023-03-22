0395756c82da971e42fbe7ba785f64c78d2a68f3
Jason Ho <hokinchong92@gmail.com>
2023-03-22 12:21:29 -0700

    assignment1

---
+++ finder-app/finder.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+FILESDIR=$1
+SEARCHSTR=$2
+num_argu=$#
+
+if [ $num_argu -eq 2 ]
+then
+    FILESDIR=$1
+    SEARCHSTR=$2
+else
+    echo "ERROR: Need 2 arguments!"
+fi
+
+if [ -d "$FILESDIR" ]
+then
+    # result=$(grep -r $SEARCHSTR $FILESDIR)
+    X=$( find $FILESDIR -type f | wc -l)
+    Y=$( grep -r $SEARCHSTR $FILESDIR | wc -l)
+else
+    echo "${FILESDIR} does not represent a directory on the filesystem"
+    exit 1
+fi
+
+echo The number of files are $X and the number of matching lines are $Y
---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+writefile=$1
+writestr=$2
+num_argu=$#
+
+if [ $num_argu -eq 2 ]
+then
+    writefile=$1
+    writestr=$2
+else
+    echo "ERROR: Need 2 arguments!"
+fi
+
+path=$(dirname $writefile)
+mkdir -p $path
+touch $writefile
+echo $writestr > $writefile
